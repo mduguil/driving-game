2246a201813b7f84d7676ed2077e862b2b00594b
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-08 18:09:45 -0700

    tryna move

--- main.js
+++ main.js
@@ -1,26 +1,48 @@
 var car = {
-  direction: 'east',
-}
+  direction: 'east'
+};
 
-var $car = document.querySelector('.car')
+var $car = document.querySelector('.car');
 
-window.addEventListener('keydown', turnCar)
+window.addEventListener('keydown', turnCar);
+window.addEventListener('keydown', move);
 
-function turnCar (event) {
+function turnCar(event) {
   if (event.keyCode === 39) {
     car.direction = 'east';
-    $car.className = 'car east'
+    $car.className = 'car east';
   }
   if (event.keyCode === 37) {
     car.direction = 'west';
-    $car.className = 'car west'
+    $car.className = 'car west';
   }
   if (event.keyCode === 38) {
     car.direction = 'north';
-    $car.className = 'car north'
+    $car.className = 'car north';
   }
   if (event.keyCode === 40) {
     car.direction = 'south';
-    $car.className = 'car south'
+    $car.className = 'car south';
   }
 }
+
+var movement = [0, 0];
+
+function move(event) {
+  var isMoving = event.keyCode === 32;
+  var carDirection = car.direction;
+  if (isMoving && carDirection === 'east') {
+    movement[0] += 3;
+  }
+  if (isMoving && carDirection === 'west') {
+    movement[0] -= 3;
+  }
+  if (isMoving && carDirection === 'south') {
+    movement[1] -= 3;
+  }
+  if (isMoving && carDirection === 'north') {
+    movement[1] += 3;
+  }
+}
+
+$car.style.transform = 'translate(' + movement[0] + 'px, ' + movement[1] + 'px)';
